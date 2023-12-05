ea666885e96d02f88849a5eb8774596b10ed1b62
Brigitte9219 <mbpadilla4@misena.edu.co>
2023-12-05 08:58:10 -0500

    Ahora se hace la ruta con boton con el evento click en html y una funcion en heroes.component.ts

--- src/app/components/heroes/heroes.component.ts
+++ src/app/components/heroes/heroes.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { HeroesService, Heroe } from 'src/app/servicios/heroes.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-heroes',
@@ -10,12 +11,17 @@ export class HeroesComponent implements OnInit{
 
   heroes:Heroe[]=[];
 
-  constructor( private _heroesService:HeroesService ){
-    console.log("constructor");
+  constructor( private _heroesService:HeroesService,
+              private router:Router ){
+    //console.log("constructor");
   }
 
   ngOnInit(){//El OnInit se utiliza cuando la pág ya esta lista para ser trabajada
     this.heroes = this._heroesService.getHeroes() ;
-    console.log(this.heroes);
+    //console.log(this.heroes);
   }
+
+    verHeroe(idx : Number){
+      this.router.navigate( ['/heroe', idx] );
+    }
 }
