d549ab6803f5253947e412cbd77cf02573adc98e
Brigitte9219 <mbpadilla4@misena.edu.co>
2023-12-05 07:26:42 -0500

    se actualiza el servicio ingresando los datos de los heroes, ubicando el metodo get y la interface, finalmente, en el componente heroes.component.ts se usa el ngOnInit para ser trabajada la pagina

--- src/app/components/heroes/heroes.component.ts
+++ src/app/components/heroes/heroes.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HeroesService, Heroe } from 'src/app/servicios/heroes.service';
 
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.css']
 })
-export class HeroesComponent {
+export class HeroesComponent implements OnInit{
 
+  heroes:Heroe[]=[];
+
+  constructor( private _heroesService:HeroesService ){
+    console.log("constructor");
+  }
+
+  ngOnInit(){//El OnInit se utiliza cuando la pág ya esta lista para ser trabajada
+    this.heroes = this._heroesService.getHeroes() ;
+    console.log(this.heroes);
+  }
 }
